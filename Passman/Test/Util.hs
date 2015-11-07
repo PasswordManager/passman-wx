c0b1ec65b7cf70537f8a0c11e81307b2387fe100
Matthew Bekkema <mbekkema97@gmail.com>
2015-11-07 16:38:05 +1100

    Limit Passman.Test.Util testcases to GT1 numbers

--- Passman/Test/Util.hs
+++ Passman/Test/Util.hs
@@ -11,8 +11,8 @@ tests = return [ testProperty "prop_toBase10" prop_toBase10
                , testProperty "prop_toFromBase" prop_toFromBase
                ]
 
-prop_toBase10 :: Natural -> Bool
-prop_toBase10 k = map ((['0'..'9']!!) . fromEnum) (toBase 10 k) == show k
+prop_toBase10 :: GT1 Natural -> Bool
+prop_toBase10 (GT1 k) = map ((['0'..'9']!!) . fromEnum) (toBase 10 k) == show k
 
 prop_toFromBase :: GT1 Natural -> Natural -> Bool
 prop_toFromBase (GT1 b) k = fromBase b (toBase b k) == k
