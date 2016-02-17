8a1984b619c0797a7b38986f7d7de69360b7c46c
Matthew Bekkema <mbekkema97@gmail.com>
2016-02-18 01:32:41 +1100

    More refactoring

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 
 import Passman.Core.PassList
@@ -13,7 +15,19 @@ import Control.Monad.Trans.Maybe (runMaybeT, MaybeT(..))
 import System.FilePath (splitFileName)
 
 import Graphics.UI.WX
+    ( (.+.), Align(..), Button, Frame, ListView, ListView(..), Prop(..)
+    , Size2D(..), Var, Window, activate, button, column, columns, command
+    , errorDialog, fileOpenDialog, fill, frame, hfill, infoDialog, listCtrlEx
+    , listViewCtrl, listViewItems, listViewSetHandler, listViewSetItems
+    , minsize, on, passwordDialog, row, set, start, text, varCreate, varCreate
+    , varGet, varSet, varUpdate, when, widget, windowReLayout, windowSetLayout
+    )
+
 import Graphics.UI.WXCore
+    ( Clipboard, EventList(..), clipboardCreate, clipboardSetData
+    , execClipBoardData, textDataObjectCreate, wxLC_REPORT, wxLC_SINGLE_SEL
+    , wxLC_REPORT
+    )
 
 data GUI = GUI { gWin          :: Frame ()
                , gListView     :: ListView PassListEntry
@@ -26,94 +40,94 @@ data GUI = GUI { gWin          :: Frame ()
 -- Main block
 
 main :: IO ()
-main = start (initGui >>= runGui)
+main = start gui
 
-initGui :: IO GUI
-initGui = do
-    gWin'          <- frame [ text := "Passman" ]
-    gListView'     <- ListView <$> listCtrlEx gWin'
+gui :: IO ()
+gui = do
+    gWin          <- frame [ text := "Passman" ]
+    gListView     <- ListView <$> listCtrlEx gWin
         (wxLC_SINGLE_SEL .+. wxLC_REPORT)
         [ columns := [ ("Info", AlignLeft, 220)
                      , ("Length", AlignRight, -1)
                      , ("Mode", AlignRight, -1)
         ]            ] <*> varCreate [] <*> pure entryToStrings
-    gGetPassword'  <- button gWin' [ text := "Get Password" ]
-    gOpenFile'     <- button gWin' [ text := "Open File" ]
-    gConfig'       <- varCreate =<< initConfig gWin'
-    gSelectedItem' <- varCreate (-1)
-    return GUI
-        {   gWin          = gWin'
-        ,   gListView     = gListView'
-        ,   gGetPassword  = gGetPassword'
-        ,   gOpenFile     = gOpenFile'
-        ,   gConfig       = gConfig'
-        ,   gSelectedItem = gSelectedItem'
-        }
+    gGetPassword  <- button gWin [ text := "Get Password" ]
+    gOpenFile     <- button gWin [ text := "Open File" ]
+    gConfig       <- varCreate =<< initConfig gWin
+    gSelectedItem <- varCreate (-1)
 
-runGui :: GUI -> IO ()
-runGui g = do
-    set (gWin g) [ on activate := flip when (windowReLayout $ gWin g) ]
-    listViewSetHandler (gListView g) (lcEvent g)
-    set (gOpenFile g) [ on command := openFile g]
-    set (gGetPassword g) [ on command := getPassword g]
-    windowSetLayout (gWin g) (column 5 [fill $ minsize (Size 400 100) $ widget $
-                                   listViewCtrl (gListView g)
-                                ,hfill $ widget (gGetPassword g)
-                                ,hfill $ widget (gOpenFile g)
-                                ])
+    let g = GUI {..}
+
+    set gWin         [ on activate := flip when (windowReLayout gWin) ]
+    set gOpenFile    [ on command := openFile g]
+    set gGetPassword [ on command := getPassword g]
+    listViewSetHandler gListView (listViewEvent g)
+    windowSetLayout gWin $ column 5 $ map (row 5)
+        [ [fill $ minsize (Size 400 100) $ widget $ listViewCtrl gListView]
+        , [hfill $ widget gGetPassword]
+        , [hfill $ widget gOpenFile]
+        ]
 
 -- Event handlers
 
-getPassword :: GUI -> IO ()
-getPassword g = do
-    let errorLoop = errorDialog (gWin g) "Incorrect password"
-                        "Incorrect password" >> getPassword g
-    selectedItem <- varGet $ gSelectedItem g
-    config <- varGet $ gConfig g
+getPasswordForEntry :: GUI -> PassListEntry -> IO ()
+getPasswordForEntry g@GUI{..} entry = do
+    let errorLoop = errorDialog' gWin "Incorrect password" >>
+                                                     getPasswordForEntry g entry
+    config <- varGet gConfig
     let hash = masterPasswordHash config
+    passwd <- passwordDialog' gWin "Please enter your password" ""
+    unless (null passwd) $ case masterPassword passwd of
+        Nothing    -> errorLoop
+        Just mpass -> if checkMasterPassword hash mpass then do
+            setClipboardText $ generatePassword entry mpass
+            infoDialog' gWin ( "The password for "
+                               ++ passListEntryInfo entry
+                               ++ " is in the clipboard. Press OK when done"
+                             )
+            setClipboardText ""
+        else errorLoop
+
+getPassword :: GUI -> IO ()
+getPassword g@GUI{..} = do
+    selectedItem <- varGet gSelectedItem
     if selectedItem < 0 then
-        errorDialog (gWin g) "No item selected" "No item selected"
+        errorDialog gWin "No item selected" "No item selected"
     else do
-        passwd <- passwordDialog (gWin g) "Please enter your password:" "Please enter your password." ""
-        unless (null passwd) $ case masterPassword passwd of
-            Nothing    -> errorLoop
-            Just mpass -> if checkMasterPassword hash mpass then do
-                entries <- varGet $ listViewItems (gListView g)
-                setClipboardText $ generatePassword (entries !! selectedItem) mpass
-                infoDialog (gWin g) "Press OK when done" "Press OK when done"
-                setClipboardText ""
-            else errorLoop
+        entries <- varGet $ listViewItems gListView
+        getPasswordForEntry g (entries !! selectedItem)
 
 openFile :: GUI -> IO ()
-openFile g = helper =<< passListDialog g
+openFile g@GUI{..} = helper =<< passListDialog g
   where
     helper :: Maybe FilePath -> IO ()
     helper Nothing = return ()
-    helper (Just path) = loadFile (gListView g) path >>= errHandler
+    helper (Just path) = loadFile gListView path >>= errHandler
     errHandler Nothing = return ()
-    errHandler (Just err) = errorDialog (gWin g) "Error" err >> openFile g
+    errHandler (Just err) = errorDialog gWin "Error" err >> openFile g
 
-lcEvent :: GUI -> EventList -> IO ()
-lcEvent g (ListItemSelected i) = varSet (gSelectedItem g) i
-lcEvent g ListDeleteAllItems = varSet (gSelectedItem g) (-1)
-lcEvent _ _ = return ()
+listViewEvent :: GUI -> EventList -> IO ()
+listViewEvent GUI{..} event = case event of
+    ListItemSelected i -> varSet gSelectedItem i
+    ListDeleteAllItems -> varSet gSelectedItem (-1)
+    _                  -> return ()
 
 -- Helper Functions
 
 setClipboardText :: String -> IO ()
-setClipboardText text = clipboardCreate >>= flip execClipBoardData helper
+setClipboardText t = clipboardCreate >>= flip execClipBoardData helper
   where
     helper :: Clipboard () -> IO ()
-    helper cl = textDataObjectCreate text >>= clipboardSetData cl >> return ()
+    helper cl = textDataObjectCreate t >>= clipboardSetData cl >> return ()
 
 passListDialog :: GUI -> IO (Maybe FilePath)
-passListDialog g = runMaybeT $ do
-    config <- liftIO $ varGet $ gConfig g
+passListDialog GUI{..} = runMaybeT $ do
+    config <- liftIO $ varGet gConfig
     let (p1,p2) = splitPassListPath $ getPassListPath config
-    path <- MaybeT $ fileOpenDialog (gWin g) True True "Open file..."
+    path <- MaybeT $ fileOpenDialog gWin True True "Open file..."
                                             [("Text Files (*.txt)", ["*.txt"])
                                             ,("All Files (*.*)",["*"])] p1 p2
-    liftIO $ updateConfig (gConfig g) (setPassListPath path)
+    liftIO $ updateConfig gConfig (setPassListPath path)
     return path
 
 getPassListPath :: Config -> Maybe FilePath
@@ -156,12 +170,22 @@ initConfig f = do
 
 initMasterPassword :: Frame () -> IO String
 initMasterPassword f = do
-    spass <- passwordDialog f "Please enter a master password:" "Please enter a master password." ""
+    spass <- passwordDialog' f "Please enter a master password" ""
     case masterPassword spass of
         Nothing    -> initMasterPassword f
         Just mpass -> hashMasterPassword mpass
 
+
+passwordDialog' :: Window a -> String -> String -> IO String
+passwordDialog' f s = passwordDialog f (s ++ ":") (s ++ ".")
+
+errorDialog' :: Window a -> String -> IO ()
+errorDialog' f = errorDialog f "Error"
+
+infoDialog' :: Window a -> String -> IO ()
+infoDialog'  f = infoDialog  f "Info"
+
 crashWithError :: Frame () -> String -> IO a
 crashWithError f m = do
-    errorDialog f "Error" m
+    errorDialog' f m
     error m
